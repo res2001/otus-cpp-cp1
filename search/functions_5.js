9433a8996ea2d00ecff91992ff102a83ce6f2eb0
res2001 <res2001@users.noreply.github.com>
2026-09-22 05:28:40 +0000

    deploy: ab474056dccff2ddd9228af61409e72cce15a49f

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['getcolorcount_0',['getColorCount',['../classColorRegistry.html#a20ca092043df63760c1a5b02ac2c9b84',1,'ColorRegistry']]],
-  ['getdocumentname_1',['getdocumentname',['../classDocumentModel.html#a4b1b01920d440e3b78766b4446efbec2',1,'DocumentModel::getDocumentName()'],['../classDocumentModel.html#a95e46d5a0b15e0a48c046cbf840f3f64',1,'DocumentModel::getDocumentName() const']]],
-  ['getprimitives_2',['getprimitives',['../classDocumentModel.html#a270a412a9e7f7fd8a9c7c034ad1036de',1,'DocumentModel::getPrimitives()'],['../classDocumentModel.html#ad7890693e5578b26ee6b8ddc13cadaae',1,'DocumentModel::getPrimitives() const']]]
+  ['getdocumentname_1',['getDocumentName',['../classDocumentModel.html#a5031b02b3c35e80e76aebaec1dddbf7d',1,'DocumentModel']]],
+  ['getprimitives_2',['getPrimitives',['../classDocumentModel.html#ad7890693e5578b26ee6b8ddc13cadaae',1,'DocumentModel']]]
 ];
